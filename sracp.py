cea114a554c95c66543f9905f5de03f594da5ea1
simonov41coder <simonov41.coder@gmail.com>
2026-05-08 22:19:45 +0700

    y

--- sracp.py
+++ sracp.py
@@ -2,12 +2,12 @@ import csv
 
 companies = []
 
-with open('Top 1000 technology companies.csv', newline="", encoding="utf-8") as f:
+with open('sample.csv', newline="", encoding="utf-8") as f:
     reader = csv.DictReader(f)
     for row in reader:
-        companies.append(row["Company"])
+        companies.append(row["title"])
 
-with open("companies.txt", "w", encoding="utf-8") as f:
+with open("companies.txt", "a", encoding="utf-8") as f:
     f.write("\n".join(companies))
 
 print("Saved", len(companies), "companies")
